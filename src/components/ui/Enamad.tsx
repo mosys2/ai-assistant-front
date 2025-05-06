4fba150c021cf8d317cef3df314efbf0597b6519
mst <mohammadsaadati117@gmail.com>
2025-05-06 19:38:34 +0330

    enamad3

--- src/components/ui/Enamad.tsx
+++ src/components/ui/Enamad.tsx
@@ -2,20 +2,12 @@
 import React from 'react';
 
 const EnamadBadge: React.FC = () => {
+    const enamadCode=`<img src="/img/pic/enamad.png" width="100" height="110"
+onclick="window.open(&quot;https://trustseal.enamad.ir/?id=607162&Code=0WpceBjNz8EgX0Da8JvTtLRy3w41de3j;, &quot;Popup&quot;,&quot;toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30&quot;)"
+alt="enamad">`
+ 
   return (
-    <a
-      href="https://trustseal.enamad.ir/?id=607162&Code=0WpceBjNz8EgX0Da8JvTtLRy3w41de3j"
-      target="_blank"
-      rel="noopener noreferrer"
-      referrerPolicy="origin"
-    >
-      <img
-        src="https://trustseal.enamad.ir/logo.aspx?id=607162&Code=0WpceBjNz8EgX0Da8JvTtLRy3w41de3j"
-        alt="نماد اعتماد الکترونیکی"
-        style={{ cursor: 'pointer' }}
-        referrerPolicy="origin"
-      />
-    </a>
+    <div style={{cursor:'pointer'}} dangerouslySetInnerHTML={{ __html: enamadCode }} />
   );
 };
 
